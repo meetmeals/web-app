a1952cabe62171dabd914d8d226da997f02fabb6
Salar Hafezi <salar.hfz.work@gmail.com>
2023-08-20 12:30:16 +0330

    feat: Add useIsMounted hook

--- src/utilities/hooks/index.ts
+++ src/utilities/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useClickOutside } from './useClickOutside';
 export { default as useWindowWidth } from './useWindowWidth';
+export { default as useIsMounted } from './useIsMounted';
