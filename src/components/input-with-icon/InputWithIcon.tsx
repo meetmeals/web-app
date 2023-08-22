3bf40d4c5984bf50cffb9fe94b7a986ad2ce950a
Salar Hafezi <salar.hfz.work@gmail.com>
2023-08-22 12:10:55 +0330

    feat: Add InputWithIcon

---
+++ src/components/input-with-icon/InputWithIcon.tsx
@@ -0,0 +1,28 @@
+import classNames from 'classnames';
+import { FieldMetaProps } from 'formik';
+import { ReactNode } from 'react';
+
+import styles from './input-with-icon.module.scss';
+
+type InputWithIconProps = {
+  icon: ReactNode;
+  meta: FieldMetaProps<string>;
+  [key: string]: any;
+};
+
+function InputWithIcon(props: InputWithIconProps) {
+  return (
+    <div className={styles['custom-input']}>
+      <span className={styles['custom-input__icon']}>{props.icon}</span>
+      <input
+        className={classNames(styles['custom-input__input'], {
+          [styles['custom-input__input--error']]:
+            props.meta.touched && props.meta.error,
+        })}
+        {...props}
+      />
+    </div>
+  );
+}
+
+export default InputWithIcon;
