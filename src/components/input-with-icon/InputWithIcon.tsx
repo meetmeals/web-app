7d5dacaab9ffd7d97691eac9664ec22ba5920d31
Salar Hafezi <salar.hfz.work@gmail.com>
2023-09-09 14:26:48 +0330

    style: use 4 spaces for tabs

--- src/components/input-with-icon/InputWithIcon.tsx
+++ src/components/input-with-icon/InputWithIcon.tsx
@@ -5,25 +5,25 @@ import { ReactNode } from 'react';
 import styles from './input-with-icon.module.scss';
 
 type InputWithIconProps = {
-  icon: ReactNode;
-  meta: FieldMetaProps<string>;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key: string]: any;
+    icon: ReactNode;
+    meta: FieldMetaProps<string>;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [key: string]: any;
 };
 
 function InputWithIcon(props: InputWithIconProps) {
-  return (
-    <div className={styles['custom-input']}>
-      <span className={styles['custom-input__icon']}>{props.icon}</span>
-      <input
-        className={classNames(styles['custom-input__input'], {
-          [styles['custom-input__input--error']]:
-            props.meta.touched && props.meta.error,
-        })}
-        {...props}
-      />
-    </div>
-  );
+    return (
+        <div className={styles['custom-input']}>
+            <span className={styles['custom-input__icon']}>{props.icon}</span>
+            <input
+                className={classNames(styles['custom-input__input'], {
+                    [styles['custom-input__input--error']]:
+                        props.meta.touched && props.meta.error,
+                })}
+                {...props}
+            />
+        </div>
+    );
 }
 
 export default InputWithIcon;
