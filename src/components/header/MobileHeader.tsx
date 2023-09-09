1a27f8a47d037d96c0c0a16e7609888643c44dfa
Salar Hafezi <salar.hfz.work@gmail.com>
2023-09-09 14:26:28 +0330

    feat: Add mobile menu

--- src/components/header/MobileHeader.tsx
+++ src/components/header/MobileHeader.tsx
@@ -1,84 +1,50 @@
-import classNames from 'classnames';
-import React from 'react';
-import { FaGlobe } from 'react-icons/fa';
-import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import MobileMenuDrawer from 'components/mobile-menu-drawer';
+import { RootState } from 'stores';
+import { setMobileDrawerOpen } from 'stores/platform';
 import { pages } from 'utilities/constants';
 
 import styles from './mobile-header.module.scss';
 
 function MobileHeader() {
-  const [activeLink, setActiveLink] = React.useState<string>(
-    pages.explore.transKey,
-  );
+    const { isMobileDrawerOpen } = useSelector(
+        (state: RootState) => state.platform,
+    );
+    const dispatch = useDispatch();
 
-  const { t } = useTranslation();
+    function handleToggleMobileMenuDrawer() {
+        dispatch(
+            setMobileDrawerOpen({
+                isMobileDrawerOpen: !isMobileDrawerOpen,
+            }),
+        );
+    }
 
-  function handleLinkClick(linkKey: string) {
-    setActiveLink(linkKey);
-  }
-
-  return (
-    <header className={styles.header}>
-      <nav className={styles['header__nav']}>
-        <section className={styles['header__nav__links']}>
-          <Link
-            to={pages.explore.alias}
-            className={classNames(styles['header__nav__links__a'], {
-              [styles['header__nav__links__a--active']]:
-                activeLink === pages.explore.transKey,
-            })}
-            onClick={() => handleLinkClick(pages.explore.transKey)}
-          >
-            <div className={styles['header__nav__links__container']}>
-              <FaGlobe />
-              <span>{t(pages.explore.transKey)}</span>
-            </div>
-          </Link>
-          <Link
-            to={pages.list.path}
-            className={classNames(styles['header__nav__links__a'], {
-              [styles['header__nav__links__a--active']]:
-                activeLink === pages.list.transKey,
-            })}
-            onClick={() => handleLinkClick(pages.list.transKey)}
-          >
-            <div className={styles['header__nav__links__container']}>
-              <FaGlobe />
-              <span>{t(pages.list.transKey)}</span>
-            </div>
-          </Link>
-          <Link
-            to={pages.contactUs.path}
-            className={classNames(styles['header__nav__links__a'], {
-              [styles['header__nav__links__a--active']]:
-                activeLink === pages.contactUs.transKey,
-            })}
-            onClick={() => handleLinkClick(pages.contactUs.transKey)}
-          >
-            <div className={styles['header__nav__links__container']}>
-              <FaGlobe />
-              <span>{t(pages.contactUs.transKey)}</span>
-            </div>
-          </Link>
-          <Link
-            to={pages.profile.path}
-            className={classNames(styles['header__nav__links__a'], {
-              [styles['header__nav__links__a--active']]:
-                activeLink === pages.profile.transKey,
-            })}
-            onClick={() => handleLinkClick(pages.profile.transKey)}
-          >
-            <div className={styles['header__nav__links__container']}>
-              <FaGlobe />
-              <span>{t(pages.profile.transKey)}</span>
-            </div>
-          </Link>
-        </section>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <MobileMenuDrawer
+                isOpen={isMobileDrawerOpen}
+                setOpen={handleToggleMobileMenuDrawer}
+            />
+            <nav className={styles['header__nav']}>
+                <Link to={pages.explore.path}>
+                    <img
+                        alt="Meetmeals logo"
+                        className={styles['header__nav__logo']}
+                        src="img/meetmeals-logo.png"
+                    />
+                </Link>
+                <img
+                    alt="Hamburger menu"
+                    className={styles['header__nav__hamburger-menu']}
+                    src="/img/icons/common/hamburger-menu.svg"
+                    onClick={handleToggleMobileMenuDrawer}
+                />
+            </nav>
+        </header>
+    );
 }
 
 export default MobileHeader;
