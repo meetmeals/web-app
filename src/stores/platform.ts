05deb4f460b93db8957b0cbd4612144083b50743
Salar Hafezi <salar.hfz.work@gmail.com>
2023-09-09 16:02:38 +0330

    feat: Add user store & mobile drawer to platform

--- src/stores/platform.ts
+++ src/stores/platform.ts
@@ -4,45 +4,60 @@ import { PlatformEnum } from 'models/local-storage';
 import { DEFAULT_LANGUAGE, MOBILE_MAX_WIDTH } from 'utilities/constants';
 
 type PlatformState = {
-  countdownTimer: number;
-  isMobile: boolean;
-  language: string;
+    countdownTimer: number;
+    isMobile: boolean;
+    language: string;
+    isMobileDrawerOpen: boolean;
 };
 
 const countdownTimer = Number(
-  localStorage.getItem(PlatformEnum.COUNTDOWN_TIMER),
+    localStorage.getItem(PlatformEnum.COUNTDOWN_TIMER),
 );
 const isMobile = window.innerWidth <= MOBILE_MAX_WIDTH;
 const platformLanguage =
-  localStorage.getItem(PlatformEnum.LANGUAGE) || DEFAULT_LANGUAGE;
+    localStorage.getItem(PlatformEnum.LANGUAGE) || DEFAULT_LANGUAGE;
 
 const initialState: PlatformState = {
-  countdownTimer,
-  isMobile,
-  language: platformLanguage,
+    countdownTimer,
+    isMobile,
+    language: platformLanguage,
+    isMobileDrawerOpen: false,
 };
 
 const platformSlice = createSlice({
-  name: 'platform',
-  initialState,
-  reducers: {
-    setCountdownTimer: (state, action: PayloadAction<number>) => {
-      localStorage.setItem(
-        PlatformEnum.COUNTDOWN_TIMER,
-        action.payload.toString(),
-      );
-      state.countdownTimer = action.payload;
+    name: 'platform',
+    initialState,
+    reducers: {
+        setCountdownTimer: (state, action: PayloadAction<number>) => {
+            localStorage.setItem(
+                PlatformEnum.COUNTDOWN_TIMER,
+                action.payload.toString(),
+            );
+            state.countdownTimer = action.payload;
+        },
+        setMobile: (state, action: PayloadAction<boolean>) => {
+            state.isMobile = action.payload;
+        },
+        setLanguage: (state, action: PayloadAction<{ language: string }>) => {
+            localStorage.setItem(
+                PlatformEnum.LANGUAGE,
+                action.payload.language,
+            );
+            state.language = action.payload.language;
+        },
+        setMobileDrawerOpen: (
+            state,
+            action: PayloadAction<{ isMobileDrawerOpen: boolean }>,
+        ) => {
+            state.isMobileDrawerOpen = action.payload.isMobileDrawerOpen;
+        },
     },
-    setMobile: (state, action: PayloadAction<boolean>) => {
-      state.isMobile = action.payload;
-    },
-    setLanguage: (state, action: PayloadAction<{ language: string }>) => {
-      localStorage.setItem(PlatformEnum.LANGUAGE, action.payload.language);
-      state.language = action.payload.language;
-    },
-  },
 });
 
-export const { setCountdownTimer, setMobile, setLanguage } =
-  platformSlice.actions;
+export const {
+    setCountdownTimer,
+    setMobile,
+    setLanguage,
+    setMobileDrawerOpen,
+} = platformSlice.actions;
 export default platformSlice.reducer;
