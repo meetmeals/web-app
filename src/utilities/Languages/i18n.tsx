415ab984977d6277f2c9b2f312f67c6e88d631d1
Salar Hafezi <salar.hfz.work@gmail.com>
2023-09-09 16:00:17 +0330

    fix: Use correct import path

--- src/utilities/Languages/i18n.tsx
+++ src/utilities/Languages/i18n.tsx
@@ -2,26 +2,30 @@ import i18next from 'i18next';
 import Backend, { HttpBackendOptions } from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
+import { PlatformEnum } from 'models/local-storage';
 import {
-  AppVersion,
-  DEFAULT_LANGUAGE,
-  SUPPORTED_LANGUAGES,
+    APP_VERSION,
+    DEFAULT_LANGUAGE,
+    SUPPORTED_LANGUAGES,
 } from 'utilities/constants';
 
+const platformLanguage =
+    localStorage.getItem(PlatformEnum.LANGUAGE) || DEFAULT_LANGUAGE;
+
 i18next
-  .use(Backend)
-  .use(initReactI18next)
-  .init<HttpBackendOptions>({
-    lng: DEFAULT_LANGUAGE,
-    backend: {
-      queryStringParams: { v: AppVersion },
-    },
-    fallbackLng: DEFAULT_LANGUAGE,
-    interpolation: {
-      escapeValue: false,
-    },
-    load: 'languageOnly',
-    supportedLngs: SUPPORTED_LANGUAGES,
-  });
+    .use(Backend)
+    .use(initReactI18next)
+    .init<HttpBackendOptions>({
+        lng: platformLanguage,
+        backend: {
+            queryStringParams: { v: APP_VERSION },
+        },
+        fallbackLng: DEFAULT_LANGUAGE,
+        interpolation: {
+            escapeValue: false,
+        },
+        load: 'languageOnly',
+        supportedLngs: SUPPORTED_LANGUAGES,
+    });
 
 export default i18next;
