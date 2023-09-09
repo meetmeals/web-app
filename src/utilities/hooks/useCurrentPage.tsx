ab848e1296f3c4ed9c5fd6e5e0d457cc0ac84c8b
Salar Hafezi <salar.hfz.work@gmail.com>
2023-09-09 15:58:05 +0330

    feat: Add useCurrentPage hook

---
+++ src/utilities/hooks/useCurrentPage.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { pages } from 'utilities/constants';
+
+function useCurrentPage() {
+    const location = useLocation();
+    const [currentPage, setCurrentPage] = React.useState<string>('');
+
+    React.useEffect(() => {
+        const path = location.pathname;
+
+        if (path.includes(pages.explore.alias!))
+            setCurrentPage(pages.explore.transKey);
+        if (path.includes(pages.packages.path))
+            setCurrentPage(pages.packages.transKey);
+        if (path.includes(pages.contactUs.path))
+            setCurrentPage(pages.contactUs.transKey);
+    }, [location]);
+
+    return currentPage;
+}
+
+export default useCurrentPage;
