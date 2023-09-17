a804f93dbd9163fdcbfaf93d542049440bebe51e
Salar Hafezi <salar.hfz.work@gmail.com>
2023-09-17 12:30:41 +0330

    feat: Add user model to store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,17 +1,34 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AuthStep, UserInterface } from 'models/common';
 
 import { UserEnum } from 'models/local-storage';
 import { DEFAULT_NATIONALITY } from 'utilities/constants';
 
 type UserState = {
+    authenticationStep: AuthStep;
     selectedNationality: string;
+    tempEmail: string;
+    isLoggedIn: boolean;
+    token: string;
+    info: UserInterface;
 };
 
 const selectedNationality =
     localStorage.getItem(UserEnum.SELECTED_NATIONALITY) || DEFAULT_NATIONALITY;
 
+const isLoggedIn = localStorage.getItem(UserEnum.IS_LOGGED_IN) === '1';
+const token = localStorage.getItem(UserEnum.TOKEN) || '';
+
+const infoString = localStorage.getItem(UserEnum.INFO);
+const info = infoString ? JSON.parse(infoString) : {};
+
 const initialState: UserState = {
+    authenticationStep: AuthStep.NONE,
     selectedNationality,
+    tempEmail: '',
+    isLoggedIn,
+    token,
+    info,
 };
 
 const userSlice = createSlice({
@@ -28,8 +45,54 @@ const userSlice = createSlice({
             );
             state.selectedNationality = action.payload.selectedNationality;
         },
+        setAuthenticating: (
+            state,
+            action: PayloadAction<{ authStep: AuthStep }>,
+        ) => {
+            state.authenticationStep = action.payload.authStep;
+        },
+        setTempEmail: (state, action: PayloadAction<{ tempEmail: string }>) => {
+            state.tempEmail = action.payload.tempEmail;
+        },
+        setLoggedIn: (
+            state,
+            action: PayloadAction<{
+                isLoggedIn: boolean;
+                token?: string;
+                info?: object;
+            }>,
+        ) => {
+            state.isLoggedIn = action.payload.isLoggedIn;
+            state.tempEmail = '';
+            localStorage.setItem(
+                UserEnum.IS_LOGGED_IN,
+                action.payload.isLoggedIn ? '1' : '0',
+            );
+            if (action.payload.token) {
+                state.token = action.payload.token;
+                localStorage.setItem(UserEnum.TOKEN, action.payload.token);
+            } else {
+                state.token = '';
+                localStorage.removeItem(UserEnum.TOKEN);
+            }
+            if (action.payload.info) {
+                state.info = action.payload.info;
+                localStorage.setItem(
+                    UserEnum.INFO,
+                    JSON.stringify(action.payload.info),
+                );
+            } else {
+                state.info = {};
+                localStorage.removeItem(UserEnum.INFO);
+            }
+        },
     },
 });
 
-export const { setSelectedNationality } = userSlice.actions;
+export const {
+    setAuthenticating,
+    setSelectedNationality,
+    setTempEmail,
+    setLoggedIn,
+} = userSlice.actions;
 export default userSlice.reducer;
