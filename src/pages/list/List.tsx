bbf0c8da6bb2dfcfe4cd826471b79e29db4ddd58
Salar Hafezi <salar.hfz.work@gmail.com>
2023-10-02 18:16:59 +0330

    fix: lint

--- src/pages/list/List.tsx
+++ src/pages/list/List.tsx
@@ -49,12 +49,12 @@ function List() {
                         offset: 0,
                         ...(!error &&
                             location.latitude && {
-                                customer_latitude: location.latitude,
-                            }),
+                            customer_latitude: location.latitude,
+                        }),
                         ...(!error &&
                             location.longitude && {
-                                customer_longitude: location.longitude,
-                            }),
+                            customer_longitude: location.longitude,
+                        }),
                     },
                     {
                         Authorization: `Bearer ${token}`,
