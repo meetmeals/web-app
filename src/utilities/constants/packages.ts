ea0c178dcae6e6d3082a725005ef2630a30c36b7
Salar Hafezi <salar.hfz.work@gmail.com>
2023-10-22 19:06:07 +0330

    chore: Add comments to package statuses

--- src/utilities/constants/packages.ts
+++ src/utilities/constants/packages.ts
@@ -2,9 +2,9 @@ export const PackageViewStatus: {
     [key: string]: { transKey: string; color: string };
 } = {
     1: { transKey: 'app.finishedInFirstHour', color: '#B8B8B8' },
-    2: { transKey: 'app.finished', color: '#B8B8B8' },
+    2: { transKey: 'app.finished', color: '#B8B8B8' }, // Nothing available
     3: { transKey: 'app.lessThanFive', color: '#FFCA1D' },
     4: { transKey: 'app.moreThanFive', color: '#45A132' },
-    5: { transKey: 'app.lastChance', color: '#FF7072' },
-    6: { transKey: 'app.saleFinished', color: '#B8B8B8' },
+    5: { transKey: 'app.lastChance', color: '#FF7072' }, // In the last hour if any packages remaining
+    6: { transKey: 'app.saleFinished', color: '#B8B8B8' }, // Delivery time has ended (15 mins rest)
 };
